cafac6784d62ea8785d368ceaaa305c5b0c83a26
KazakovMaksim <kazakovmaksimi@gmail.com>
2021-02-21 18:51:11 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    
+    let arr = [];
+    if (n > 0) {
+        arr = String(n).split("");
+        arr = arr.reverse();
+        n = +arr.join("");
+    }
+    if (n < 0) {
+        arr = String(n).split("");
+        arr.shift();
+        arr.reverse();
+        n = +arr.join("");
+    }
+    
+    return n;    
 }
